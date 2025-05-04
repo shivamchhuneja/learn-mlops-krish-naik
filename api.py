e2601f1a9685ab54bf4ad7230ae9e722d5a508c8
Shivam <shivam@brandsutra.org>
2025-05-04 16:46:14 +0530

    Wrapping flask, moving to MLFlow

--- api.py
+++ api.py
@@ -29,16 +29,34 @@ def get_item(item_id):
     return jsonify(item)
 
 ## Post: create a new task
-@app.route('/items'.methods['POST'])
+@app.route('/items',:methods['POST'])
 def create_item():
     if not request.json or not 'name' in request.json:
         return jsonify({"error":"item not found"})
     new_item={
         "id": items[-1]["id"] + 1 if items else 1
-        "name":request.json
-
-
+        "name":request.json['name'],
+        "description":requst.json["description"]
     }
+    items.append(new_item)
+    return jsonify(new_item)
+
+
+# Put: Update an item
+@app.route('items/<int:item_id>',methods=['PUT'])
+def update_item(item_id):
+    item = next((item for item in items it item["id"] == item_id), None)
+    if item is None:
+        return jsonify({"error": "Item not found"})
+    item['name'] = request.json.get('name', item['name'])
+    item['description'] = request.json.get('description', item['description'])
+    return jsonify(item)
+
+@app.route('items/<int:item_id',methods=['DELETE'])
+def delete_item(item_id):
+    global items
+    items = [item for item in items if["id"] != item_id]
+    return jsonify({"result": "Item Deleted"}) 
 
 
 
